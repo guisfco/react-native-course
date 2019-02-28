da64a33894f883696776391dfeeda82544032fe4
Guilherme dos Santos Francisco <guilherme.francisco@cwi.com.br>
2019-02-28 17:55:54 -0300

    Primeiro app finalizado

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
+    state = {
+        albums: []
+    }
+
     componentWillMount() {
-        console.log('Method was called')
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />
+        );
     }
 
     render() {
         return (
-            <View>
-                <Text>Album list!!</Text>
-            </View >
+            <ScrollView>
+                {this.renderAlbums()}
+            </ScrollView >
         );
     }
 }
