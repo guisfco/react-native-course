71a7e42606176baac00cc5fda5aa5782a57462a3
Guilherme dos Santos Francisco <guilherme.francisco@cwi.com.br>
2019-03-04 16:51:57 -0300

    Segundo app (Auth) finalizado

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 import firebase from 'firebase';
 
 class App extends Component {
+
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -15,13 +17,38 @@ class App extends Component {
       storageBucket: 'auth-react-native-tutori-dbd5c.appspot.com',
       messagingSenderId: '790573668638'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+              </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />
+      default:
+        return <Spinner size="large" />
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
